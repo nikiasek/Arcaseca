dff40071ba9b0206fad1df6942ffb91a82829fd7
nikiasek <nikiasek50@gmail.com>
2023-12-01 21:10:38 +0100

    authentication
    - user is able to login
    - user is redirected to login if he's not logged in

--- Arcaseca/mainArcaseca/dashboard/views.py
+++ Arcaseca/mainArcaseca/dashboard/views.py
@@ -3,9 +3,11 @@ from django.template import loader
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url="login")
 def dashboard(request):
     return render(request, "index.html", {})
 
